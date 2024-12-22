b422aac1213c28b30d197ff0f4f20460b976e15d
taekwon-dev <devtkwon@gmail.com>
2024-12-22 14:14:22 +0900

    initial commit

---
+++ src/test/java/io/hhplus/architecture/ArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
